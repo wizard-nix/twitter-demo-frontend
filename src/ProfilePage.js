65821daaf76af71ec958c3afbfc5fef8c894bcec
Art Urov <shnurov.art@gmail.com>
2018-06-23 16:54:56 +0400

    refactor structure

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Helmet from "react-helmet";
 import Header from "./Header";
@@ -11,20 +10,6 @@ import TrendSideBar from "./TrendSideBar";
 import CopyrightSideBar from "./CopyrightSideBar";
 import RecFollow from "./RecFollow";
 import Media from "./Media";
-=======
-import React from 'react';
-import Helmet from 'react-helmet';
-import Header from './components/Header';
-import ProfileImg from './components/ProfileImg';
-import ProfileStat from './components/ProfileStat';
-import ProfileBio from './components/ProfileBio';
-import Stream from './components/Stream';
-import ProfileSideBar from './components/ProfileSideBar';
-import TrendSideBar from './components/TrendSideBar';
-import CopyrightSideBar from './components/CopyrightSideBar';
-import RecFollow from './components/RecFollow';
-import Media from './components/Media';
->>>>>>> 45e4609958819edfe7317606fc89e21197bc981b
 
 const ProfilePage = () => (
   <div>
@@ -32,16 +17,10 @@ const ProfilePage = () => (
       title="Every Interaction (@EveryInteract) on Twitter"
       meta={[
         {
-<<<<<<< HEAD
           name: "description",
           content:
             "The latest Tweets from Every Interaction (@EveryInteract). A user-experience design studio in London. We specialise in making the complex simple, the confusing intuitive and the perplexing obvious. London, UK"
         }
-=======
-          name: 'description',
-          content: 'The latest Tweets from Every Interaction (@EveryInteract). A user-experience design studio in London. We specialise in making the complex simple, the confusing intuitive and the perplexing obvious. London, UK',
-        },
->>>>>>> 45e4609958819edfe7317606fc89e21197bc981b
       ]}
     />
     <Header />

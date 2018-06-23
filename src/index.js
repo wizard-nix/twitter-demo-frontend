6718c58decc75b3486c68064f1883fe56b831154
Art Urov <31142045+wizard-nix@users.noreply.github.com>
2018-06-23 11:43:17 +0300

    Revert "new version"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
-import 'normalize.css';
-import 'flexboxgrid2';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render (<App />, document.getElementById ('root'));
-registerServiceWorker ();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

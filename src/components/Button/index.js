384f4558101575cd476ab680313ead28f1743aa6
Art Urov <shnurov.art@gmail.com>
2018-06-23 10:40:06 +0400

    add export styled-component

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.button`
+  font-size: 14px;
+  color: #fff;
+  background: #1da1f2;
+  border-radius: 15px;
+  border: none;
+  cursor: pointer;
+  border-radius: 100px;
+  font-weight: bold;
+  width: 72px;
+  height: 32px;
+`;
